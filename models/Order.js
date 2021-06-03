e193a8ddba62d72229ef132521328344047b77c3
MohNooR1496 <62880535+MohammedNoor1496@users.noreply.github.com>
2021-06-04 01:41:18 +0300

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -15,7 +15,8 @@ const order = new Schema({
     },
     products_id: [
         {
-            type: mongoose.Schema.Types.ObjectId, ref: 'Prouduct'
+            type: mongoose.Schema.Types.ObjectId, ref: 'Prouduct',
+            quantity : Number
         }
     ],
     destination_long: {
