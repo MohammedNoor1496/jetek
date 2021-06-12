c87599d3caa3cc28608332d968b424cc767d397f
MohNooR1496 <62880535+MohammedNoor1496@users.noreply.github.com>
2021-06-12 16:35:39 +0300

    update front test point of sell front

---
+++ build/precache-manifest.60d141a67c8838d19114a0a36864b646.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4408f1c4be5acff32228866feb551a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3827dffb0ead6831e69",
+    "url": "/static/css/2.d2ebfe18.chunk.css"
+  },
+  {
+    "revision": "5e72e62e21983fd4e572",
+    "url": "/static/css/main.bf2bcbe5.chunk.css"
+  },
+  {
+    "revision": "e3827dffb0ead6831e69",
+    "url": "/static/js/2.81835451.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.81835451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e72e62e21983fd4e572",
+    "url": "/static/js/main.bcc59976.chunk.js"
+  },
+  {
+    "revision": "b7f25be1b0d205ef19b7",
+    "url": "/static/js/runtime-main.2b2479c5.js"
+  }
+]);
