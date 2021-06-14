cae8b390d7e67ed919e37f60956665240f570831
MohNooR1496 <62880535+MohammedNoor1496@users.noreply.github.com>
2021-06-14 23:41:17 +0300

    Update SpAdmin.js

--- models/SpAdmin.js
+++ models/SpAdmin.js
@@ -42,7 +42,7 @@ const spAdminSchema = new Schema({
     },
     cpImage: {
         type: String,
-        required: true,
+        required: false,
     },
     isConfirmed: {
         type: Boolean,
