15556cf7c9ff0bf91976f3bee1ab05222cfbf69b
MohNooR1496 <62880535+MohammedNoor1496@users.noreply.github.com>
2021-06-16 12:09:00 +0300

    add

---
+++ build/precache-manifest.53b97675389f5ab5aeddc0b6733f0063.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d7b509763cabeea540521649eebd6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4303f61f421ad679ad25",
+    "url": "/static/css/2.e4b613c5.chunk.css"
+  },
+  {
+    "revision": "55ce99b0f120b588c492",
+    "url": "/static/css/main.bf2bcbe5.chunk.css"
+  },
+  {
+    "revision": "4303f61f421ad679ad25",
+    "url": "/static/js/2.91c66d4a.chunk.js"
+  },
+  {
+    "revision": "9efecc6e366a89e935d3722c67df4853",
+    "url": "/static/js/2.91c66d4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55ce99b0f120b588c492",
+    "url": "/static/js/main.205435b7.chunk.js"
+  },
+  {
+    "revision": "b7f25be1b0d205ef19b7",
+    "url": "/static/js/runtime-main.2b2479c5.js"
+  }
+]);
