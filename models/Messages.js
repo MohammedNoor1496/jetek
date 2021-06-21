02404c96152706ca421cb0f1741d91e35b248c3e
MohNooR1496 <62880535+MohammedNoor1496@users.noreply.github.com>
2021-06-21 16:54:49 +0300

    Update Messages.js

--- models/Messages.js
+++ models/Messages.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const MessageSchema = mongoose.Schema({
+const {Schema} = mongoose;
+const MessageSchema =new Schema({
 
     senderPhone: {
         type: mongoose.Schema.Types.ObjectId,
